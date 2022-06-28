4d7157c1ead83a3ff64c080568a986407c1f595f
Shadat Rahman <shadat.rahman.464@gmail.com>
2022-06-29 01:13:36 +0600

    get function dio

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -12,4 +12,14 @@ class HTTPService {
     _appConfig = GetIt.instance.get<AppConfig>();
     _baseUrl = _appConfig!.COIN_API_BASE_URL;
   }
+
+  Future<Response?> get(String _path) async {
+    try {
+      String _url = "$_baseUrl$_path";
+      Response response = await dio.get(_url);
+      return response;
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 }
