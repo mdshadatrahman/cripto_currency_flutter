01f3a2b15c75ba5e0e3a4a03059d166841ec6b4b
Shadat Rahman <shadat.rahman.464@gmail.com>
2022-06-29 01:00:44 +0600

    getit instance--> registersingletone, get

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
-import 'package:cripto_currency/pages/home_page.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
+import 'package:cripto_currency/model/app_config.dart';
+import 'package:cripto_currency/pages/home_page.dart';
+import 'package:cripto_currency/services/http_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
+  registerHTTPService();
   runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString("assets/config/main.json");
+  Map _configData = jsonDecode(_configContent);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(COIN_API_BASE_URL: _configData['COIN_API_BASE_URL']),
+  );
+}
+
+void registerHTTPService(){
+  GetIt.instance.registerSingleton<HTTPService>(
+    HTTPService(),
+  );
 }
 
 class MyApp extends StatelessWidget {
